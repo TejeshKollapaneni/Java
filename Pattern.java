04fc18c948118bb8a834cc1770b7d91a0a6fa51e
TejeshKollapaneni <83062145+TejeshKollapaneni@users.noreply.github.com>
2022-11-19 17:46:40 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+class Pattern //class name "large"
+{
+	public static void main(String[] args)//main method
+	{
+		int i,j;//variable declaration
+		for(i=5;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("*");//print *
+			}
+			System.out.println();//move on next line
+		}
+	}
+}	
