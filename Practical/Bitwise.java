61c4b7c02a2ebc416395dd90c6b3c1091989e84e
TejeshKollapaneni <83062145+TejeshKollapaneni@users.noreply.github.com>
2022-11-20 11:53:25 +0530

    Create Bitwise.java

---
+++ Practical/Bitwise.java
@@ -0,0 +1,14 @@
+class Bitwise
+{
+	public static void main(String[] args)
+	{
+		int x,y,z;
+		x=10;
+		y=4;
+		z=2;
+		System.out.println(x&(y&z));
+		System.out.println(x|(y|z));
+		System.out.println(x<<(y|z));
+		System.out.println(x>>(y^z));
+	}
+}
