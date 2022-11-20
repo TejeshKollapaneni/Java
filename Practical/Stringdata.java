1ae8c41a61e1beed4b67934cb388f9b3ded8b184
TejeshKollapaneni <83062145+TejeshKollapaneni@users.noreply.github.com>
2022-11-20 12:06:18 +0530

    Create Stringdata.java

---
+++ Practical/Stringdata.java
@@ -0,0 +1,12 @@
+class Stringdata
+{
+	public static void main(String[] args)
+	{
+		String name="REGI";
+		String name1="SUTHA";
+		String name2="SAKI";
+		System.out.println(name);
+		System.out.println(name1);
+	    System.out.println(name2);
+	}
+}
