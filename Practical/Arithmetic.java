621ae2275c2a20a117e3db9c9da76bc55f5d1236
TejeshKollapaneni <83062145+TejeshKollapaneni@users.noreply.github.com>
2022-11-20 11:52:37 +0530

    Create Arithmetic.java

---
+++ Practical/Arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic
+{
+	public static void main(String[] args)
+	{
+		int a,b,c,d,x,y,z;
+		x=10;
+		y=4;
+		z=2;
+		a=x+(y+z);
+		b=y+(x-z);
+		c=x*(y*z);
+		d=x%(y/z);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
