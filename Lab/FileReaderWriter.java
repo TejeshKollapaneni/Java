3563559b6de8e9e51077c2375d2984c21a1acd73
TejeshKollapaneni <83062145+TejeshKollapaneni@users.noreply.github.com>
2022-11-28 19:28:17 +0530

    Add files via upload

---
+++ Lab/FileReaderWriter.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class FileReaderWriter
+{
+	public static void main(String[] args)throws IOException
+	{
+		FileWriter fw=new FileWriter("d:\\file1.txt");//specified destination to write the file
+		fw.write("Hai Everyone");
+		FileReader fr=new FileReader("d:\\file1.txt");//to read the file from the specified destination
+		int i;
+		while((i=fr.read())!=-1)//read the data
+			System.out.println((char)i);
+		fr.close();//close the file
+		fw.close();//close the file
+		System.out.println("Success");
+	}
+}
